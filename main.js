9247d2b02e90821f066a43a5fbead18a0aacef39
ajartijaouad <ajartijaouad14@gmail.com>
2025-11-11 14:36:19 +0100

    Implement square root calculation and update HTML structure for input

--- main.js
+++ main.js
@@ -1,29 +1,9 @@
-// ch1 = prompt("donner une chaine ");
-// ch2 = prompt("donner une chaine ");
-// if (ch1 == ch2){
-//     alert("les chain son ideontique")
-// }else {
-//     alert("leschine sont pas idontique")
-// }
-// function compare(){
-//     let c1 = document.getElementById("ch1").value
-//     let c2 = document.getElementById("ch2").value
-//     let r  = document.getElementById("rs");
-//     if ( c1 == c2 ){
-//         r.textContent = "les chain son indique "
-//     }else{
-//         r.innerHTML =  "son pa indique "
-//     }
+function affichier(){
+let numbre = document.getElementById("n").value
+let h = Math.sqrt(Math.abs(numbre));
+let res = document.querySelector("span")
 
-// }
 
-// function teste(){
-//      let c1 = document.getElementById("ch1").value;
-//      if (c1 =='JAvascript'){
-//         alert('barvo');
-//      }else{
-//         alert('Atontion voulez vou reussi')
-        
-//      }
+res.innerHTML = `le racine de ${ numbre} est ${h}`
 
-//  }
+}
