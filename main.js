e207190f31663b40ce0956336a89230911dc8e04
ajartijaouad <ajartijaouad14@gmail.com>
2025-11-11 12:26:55 +0100

    Refactor HTML form structure and update JavaScript comparison logic

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-ch1 = prompt("donner une chaine ");
-ch2 = prompt("donner une chaine ");
-if (ch1 == ch2){
-    alert("les chain son ideontique")
-}else {
-    alert("leschine sont pas idontique")
+// ch1 = prompt("donner une chaine ");
+// ch2 = prompt("donner une chaine ");
+// if (ch1 == ch2){
+//     alert("les chain son ideontique")
+// }else {
+//     alert("leschine sont pas idontique")
+// }
+function compare(){
+    let c1 = document.getElementById("ch1").value
+    let c2 = document.getElementById("ch2").value
+    let r  = document.getElementById("rs");
+    if ( c1 == c2 ){
+        alert ( "les chain son indique ")
+
+    }else{
+alert ( "les chain son indique ")
+    }
+
 }
