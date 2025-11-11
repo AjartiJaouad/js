f76dccddf3b5abfad0d39e7709763d00d93719c9
ajartijaouad <ajartijaouad14@gmail.com>
2025-11-11 12:14:37 +0100

    Add initial HTML structure and basic JavaScript functionality for string comparison

---
+++ main.js
@@ -0,0 +1,7 @@
+ch1 = prompt("donner une chaine ");
+ch2 = prompt("donner une chaine ");
+if (ch1 == ch2){
+    alert("les chain son ideontique")
+}else {
+    alert("leschine sont pas idontique")
+}
