5b152cd643b0fee63842617d1effcde7e16bd8ba
ajartijaouad <ajartijaouad14@gmail.com>
2025-11-11 14:44:34 +0100

    Remove commented-out square root function and add new HTML file for random number generation

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-function affichier(){
-let numbre = document.getElementById("n").value
-let h = Math.sqrt(Math.abs(numbre));
-let res = document.querySelector("span")
+// function affichier(){
+// let numbre = document.getElementById("n").value
+// let h = Math.sqrt(Math.abs(numbre));
+// let res = document.querySelector("span")
 
 
-res.innerHTML = `le racine de ${ numbre} est ${h}`
-
-}
+// res.innerHTML = `le racine de ${ numbre} est ${h}
