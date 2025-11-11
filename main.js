9e7ab2c1527f8708bc74647717e118956f95e688
ajartijaouad <ajartijaouad14@gmail.com>
2025-11-11 14:12:58 +0100

    Refactor HTML form and update JavaScript function for string input validation

--- main.js
+++ main.js
@@ -5,15 +5,24 @@
 // }else {
 //     alert("leschine sont pas idontique")
 // }
-function compare(){
-    let c1 = document.getElementById("ch1").value
-    let c2 = document.getElementById("ch2").value
-    let r  = document.getElementById("rs");
-    if ( c1 == c2 ){
-        alert ( "les chain son indique ")
+// function compare(){
+//     let c1 = document.getElementById("ch1").value
+//     let c2 = document.getElementById("ch2").value
+//     let r  = document.getElementById("rs");
+//     if ( c1 == c2 ){
+//         r.textContent = "les chain son indique "
+//     }else{
+//         r.innerHTML =  "son pa indique "
+//     }
 
-    }else{
-alert ( "les chain son indique ")
-    }
+// }
 
-}
+function teste(){
+     let c1 = document.getElementById("ch1").value;
+     if (c1 =='JAvascript'){
+        alert('barvo');
+     }else{
+        alert('ereur voulez vou reussi')
+     }
+
+ }
