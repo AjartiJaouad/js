3a9f5956ff69a19a7e10b033ffc7b58bb94cd933
ajartijaouad <ajartijaouad14@gmail.com>
2025-11-12 12:15:29 +0100

    Add user manipulation functions to main.js

--- main.js
+++ main.js
@@ -11,3 +11,14 @@ const user = ["Paule", "ana", "amine","vicore"]
 
 console.log(user.length)
 console.log(user[user.length -1])
+
+// pouer ajouter un user ou tableux
+user.push("jaouad")
+console.log(user);
+// enleve le derinier element de tableux
+ user.pop()
+ console.log(user)
+//  indexof  routern lindes de element 
+console.log(user.indexOf("amine"))
+// le type de tableux est un object
+console.log(typeof(user));
