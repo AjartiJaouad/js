b7ead3f79b37a8ca67bbdfd44b885967b9a530cb
ajartijaouad <ajartijaouad14@gmail.com>
2025-11-12 16:03:21 +0100

    Remove unused HTML files and clean up main.js by deleting commented-out code

--- main.js
+++ main.js
@@ -1,24 +1,2 @@
-// function affichier(){
-// let numbre = document.getElementById("n").value
-// let h = Math.sqrt(Math.abs(numbre));
-// let res = document.querySelector("span")
-
-
-// res.innerHTML = `le racine de ${ numbre} est ${h}
-// les object 
-// les tableaux 
-const user = ["Paule", "ana", "amine","vicore"]
-
-console.log(user.length)
-console.log(user[user.length -1])
-
-// pouer ajouter un user ou tableux
-user.push("jaouad")
-console.log(user);
-// enleve le derinier element de tableux
- user.pop()
- console.log(user)
-//  indexof  routern lindes de element 
-console.log(user.indexOf("amine"))
-// le type de tableux est un object
-console.log(typeof(user));
+let taitl = document.querySelector("h1");
+console.log(taitl);
