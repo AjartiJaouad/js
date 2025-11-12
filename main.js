b10061dd373a67fab93988d882e6860034541f07
ajartijaouad <ajartijaouad14@gmail.com>
2025-11-12 16:51:07 +0100

    Fix typo in variable declaration and update header text and background color

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-let taitl = document.querySelector("h1");
-console.log(taitl);
+const taitle = document.querySelector("h1")
+// modfier le taxte 
+taitle.textContent ="le taxte a été changer "
+// changer la colore par js
+taitle.style.backgroundColor = 'red'
+console.log(window)
