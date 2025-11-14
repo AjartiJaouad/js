ce9f1458dfa1399560f0ba096cebbad20aba8124
ajartijaouad <ajartijaouad14@gmail.com>
2025-11-14 12:21:08 +0100

    Add new HTML file and implement basic conditional logic in main.js

---
+++ les_bas_js/main.js
@@ -0,0 +1,7 @@
+const nb1 = 100 ;
+const nb2 = 20 ;
+if( nb1>nb2){
+    console.log('condtion true ,instruction execute')
+}else{
+    console.log('condtions fols ')
+}
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const taitle = document.querySelector("h1")
-// modfier le taxte 
-taitle.textContent ="le taxte a été changer "
-// changer la colore par js
-taitle.style.backgroundColor = 'red'
-console.log(window)
+// const taitle = document.querySelector("h1")
+// // modfier le taxte 
+// taitle.textContent ="le taxte a été changer "
+// // changer la colore par js
+// taitle.style.backgroundColor = 'red'
+// console.log(window)
+// les evinement 
