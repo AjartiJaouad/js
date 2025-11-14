e275fb762cdb05e17ab96c9993e284b0139b871d
ajartijaouad <ajartijaouad14@gmail.com>
2025-11-14 15:08:58 +0100

    Remove unused HTML file and clean up main.js by commenting out old code

--- les_bas_js/main.js
+++ les_bas_js/main.js
@@ -1,7 +1,28 @@
-const nb1 = 100 ;
-const nb2 = 20 ;
-if( nb1>nb2){
-    console.log('condtion true ,instruction execute')
-}else{
-    console.log('condtions fols ')
+// const nb1 = 100 ;
+// const nb2 = 20 ;
+// if( nb1>nb2){
+//     console.log('condtion true ,instruction execute')
+// }else{
+//     console.log('condtions fols ')
+// }
+// let isLoggeg = false ;
+// if(isLoggeg){
+//     console.log("test")
+// }
+
+// condtion if else 
+let age = 19;
+
+if (age >= 18) {
+    console.log("majour")
+} else if (age >= 0 && age < 18) {
+    console.log("mineur")
+}
+let role = "j";
+if (role === "admin") {
+    console.log("admin")
+} else if (role === "employer") {
+    console.log("Emplyer")
+} else {
+    console.log("hhhh")
 }
